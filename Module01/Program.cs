847cc787ca3539e9cb93b46dcc132939450d468b
Vasquez-Terry <tterry@mmc.edu>
2018-01-15 10:12:18 -0600

    Add project files.

---
+++ Module01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Module01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Pause();
+        }
+
+        static void Pause()
+        {
+            Console.WriteLine("press Enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
